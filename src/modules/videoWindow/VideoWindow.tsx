e1d789ca3ecb679cc5b39ba02c123b164734ed6a
Aleksei Gurin <dmb2009isr@bk.ru>
2022-08-27 09:59:26 +0700

    add video-js

--- src/modules/videoWindow/VideoWindow.tsx
+++ src/modules/videoWindow/VideoWindow.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
 const VideoWindow = () => {
-  return (
-    <div className="video-window-container">
-        <video controls width="400" height="400" src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"></video>
-    </div>
-  )
+    return (
+        <div className="video-window-container">
+            <video
+                id="my-video"
+                className="video-js"
+                controls
+                preload="auto"
+                width="640"
+                height="264"
+                poster="MY_VIDEO_POSTER.jpg"
+                data-setup="{}"
+            >
+                <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+                        type="video/mp4"/>
+            </video>
+            <script src="https://vjs.zencdn.net/7.20.2/video.min.js"></script>
+        </div>
+    )
 }
 
 export default VideoWindow;
