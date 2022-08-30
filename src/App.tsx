defa09f3747210f2310e4378e348f267c0870a9b
Aleksei Gurin <dmb2009isr@bk.ru>
2022-08-30 16:58:10 +0700

    work with events

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './modules/counter/Counter';
-import './App.css';
 import VideoWindow from "./modules/videoWindow/VideoWindow";
 import EventListContainer from "./modules/videoWindow/EventListContainer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <VideoWindow />
         <EventListContainer />
         {/*<Counter />*/}
-      </header>
     </div>
   );
 }
--- src/modules/videoWindow/EventListContainer.tsx
+++ src/modules/videoWindow/EventListContainer.tsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import eventList from "./eventList.json"
+import moment from "moment";
+const sortedListOfEvents = eventList.events.sort((a, b) => a.timestamp - b.timestamp )
 
-const arrayEvents = eventList.events
-
+type EventType = {
+    id: number,
+    timestamp: number,
+    duration: number
+}
+const handleEventClick = (e: React.MouseEvent ) => {
+    console.log('event',e)
+    console.log('currentTarget',e.currentTarget)
+}
+const renderEvent = (event: EventType) => {
+const totalMs = event.timestamp;
+const min = Math.floor(Number(totalMs)/60000);
+const sec = Math.floor(Number(totalMs)/1000);
+const ms = totalMs.toString().slice(-3)
+const totalTimeString = `${min} : ${sec} : ${ms}`
+// console.log('totalMs',totalMs)
+// console.log('min',min)
+// console.log('sec',sec)
+// console.log('ms',ms)
+// console.log('totalTimeString',totalTimeString)
+    return (
+        <div key={event.id} id={event.id.toString()} className="event" onClick={handleEventClick}>
+            <div>{`id: ${event.id}`}</div>
+            <div>{`timestamp: ${totalTimeString}`}</div>
+            <div>{`duration: ${event.duration}`}</div>
+        </div>
+    )
+}
 const EventListContainer = () => {
-    console.log('arrayEvents',arrayEvents)
+    //console.log('sortedListOfEvents',sortedListOfEvents)
     return (
         <div className="event-list-container">
-            {arrayEvents.map((event) => (
-                <div key={event.id} className="event">
-                    <div>{`id: ${event.id}`}</div>
-                    <div>{`timestamp: ${event.timestamp}`}</div>
-                    <div>{`duration:${event.duration}`}</div>
-                </div>
-            )) }
+            {sortedListOfEvents.map((event) => renderEvent(event))}
         </div>
     )
 }
