b93b73a8aca709d16f422fe84313b1a01be81700
Aleksei Gurin <dmb2009isr@bk.ru>
2022-09-04 11:48:05 +0700

    work with video events

--- src/modules/videoWindow/VideoWindow.tsx
+++ src/modules/videoWindow/VideoWindow.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import { deleteEventVideo, selectEvent } from "../../store/slices/eventVideoSlice";
+import {deleteEventVideo, selectEvent, selectEventVideo} from "../../store/slices/eventVideoSlice";
 import videojs from 'video.js';
 import 'video.js/dist/video-js.css';
 import eventList from "./eventList.json"
@@ -11,22 +11,20 @@ const VideoWindow = () => {
     const selectedEvent = useAppSelector(selectEvent)
     const rectangleStyle = selectedEvent.selectedEventVideo.zone;
     const { duration } = selectedEvent.selectedEventVideo;
-    const videoRef = React.useRef(null);
-    const playerRef = React.useRef(null);
-    const videoJsOptions = {
-        autoplay: true,
-        controls: true,
-        responsive: true,
-        fluid: true,
-        sources: [{
-            src: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-            type: 'video/mp4'
-        }]
-    };
-
+    const [currentTimer, setCurrentTimer] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const timestamp = selectedEvent.selectedEventVideo && selectedEvent.selectedEventVideo.timestamp && Number((selectedEvent.selectedEventVideo.timestamp / 1000).toFixed(3));
+    const isPlayingListener = (e: object) => {
+        console.log('isPlaying')
+        setIsPlaying(true);
+    }
+    const isPausedListener = (e: object) => {
+        console.log('isPaused')
+        setIsPlaying(false);
+    }
+    const playingTimeIntervalRef = useRef<NodeJS.Timeout>();
     useEffect(() => {
-        const videoElement = document.getElementById('my-video') as HTMLMediaElement
-        const timestamp = selectedEvent.selectedEventVideo && selectedEvent.selectedEventVideo.timestamp && Number((selectedEvent.selectedEventVideo.timestamp / 1000).toFixed(3));
+        const videoElement = document.getElementById('my-video') as HTMLMediaElement;
         if (timestamp) {
             // const eventVideoElement = document.getElementById(selectedEvent.selectedEventVideo.id.toString()) as HTMLElement;
             // console.log('eventVideoElement',eventVideoElement)
@@ -45,13 +43,32 @@ const VideoWindow = () => {
 
         }
         console.log('selectedEvent',selectedEvent)
-        console.log('video',videoElement.currentTime)
     },[selectedEvent]);
     useEffect(() => {
-        // console.log('render')
-        // const video = document.getElementById('my-video')
-        // console.log(video.currentTime)
-    })
+        console.log('render')
+        const videoElement = document.getElementById('my-video') as HTMLMediaElement;
+        videoElement.addEventListener('playing', isPlayingListener)
+        videoElement.addEventListener('pause', isPausedListener)
+        console.log('videoElement',videoElement);
+        if (isPlaying) {
+            playingTimeIntervalRef.current = setInterval(() => {
+                eventList.events.forEach((item) => {
+                    let sec = Math.floor(Number(item.timestamp)/1000);
+                    console.log('item', item)
+                    console.log('sec', sec)
+                    console.log('videoElement', videoElement.currentTime)
+                    if (videoElement && videoElement.currentTime > sec) {
+                        console.log('green  rectangle', item)
+                        dispatch(selectEventVideo(item))
+                    }
+                })
+            },1000)
+        } else {
+            console.log('else')
+            clearInterval(playingTimeIntervalRef.current as NodeJS.Timeout);
+        }
+
+    },[isPlaying])
     return (
         <div className="video-window-container">
             <video
@@ -61,9 +78,8 @@ const VideoWindow = () => {
                 preload="auto"
                 width="640"
                 height="264"
-                poster="MY_VIDEO_POSTER.jpg"
+                poster="../../../public/poster.png"
                 data-setup="{}"
-                autoPlay
             >
                 <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
                         type="video/mp4"/>
